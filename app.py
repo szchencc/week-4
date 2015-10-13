94aed39df755b4b434f5c53706f1100850d890f3
szchencc <xc2324@columbia.edu>
2015-10-14 06:19:13 +0800

    week_4 02
    can not update data when click the buttom

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import time
 import sys
 
 import pyorient
+import random
 
 app = Flask(__name__)
 
@@ -17,8 +18,8 @@ def index():
 def getData():
 	
 	client = pyorient.OrientDB("localhost", 2424)
-	session_id = client.connect("root", "password")
-	db_name = "property_test"
+	session_id = client.connect("root", "admin")
+	db_name = "soufun"
 	db_username = "admin"
 	db_password = "admin"
 
@@ -38,6 +39,8 @@ def getData():
 	query = 'SELECT FROM Listing WHERE latitude BETWEEN {} AND {} AND longitude BETWEEN {} AND {}'
 
 	records = client.command(query.format(lat1, lat2, lng1, lng2))
+        random.shuffle(records)
+        records = records[:100]
 
 	numListings = len(records)
 	print 'received ' + str(numListings) + ' records'
